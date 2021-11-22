91af232467988deff7804ea65de2585cf410f4a2
Vannu25 <92512520+Vannu25@users.noreply.github.com>
2021-11-21 23:19:33 -0800

    Add files via upload

---
+++ LoginTest.java
@@ -0,0 +1,22 @@
+package TestCasesPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+        Thread.sleep(1000);
+        System.out.println("details added on login page");
+        driver.close();
+	}
+
+}
